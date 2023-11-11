cc76031995232e46ae02fdaf4f45eee0e5a8d5bd
Shreesha_Voniyadka <shreesha@assertion.cloud>
2023-11-11 15:03:00 +0530

    11-11-2023

---
+++ src/App.js
@@ -0,0 +1,51 @@
+// import logo from './logo.svg';
+import Avatar from '../src/Assets/myAvatar.svg'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import Navibar from '../src/Navigationbar/navigationbar'
+import Projects from './components/Projects/project'
+import Resume from './components/Resume/resume'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <header className="newlogo">
+        <img src={Avatar} className="App-logo" alt="logo" />
+      </header> 
+      <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/navbar" element={<Navibar />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import Button from "react-bootstrap/Button";
+import Navigbar from '../../Navigationbar/navigationbar'
+
+function Home() {
+  return (
+    <div>
+      {/* Your Home component content */}
+      <h1> Portfolio </h1>
+      {/* <ThemeProvider prefixes={{ btn: 'my-btn' }}>
+        <Button variant="primary">My Button</Button>
+      </ThemeProvider>
+      <Button bsPrefix="super-btn" variant="primary">
+        Super button
+      </Button> */}
+      <Navigbar />
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+
+
