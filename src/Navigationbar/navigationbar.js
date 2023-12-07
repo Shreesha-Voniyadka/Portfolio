e9d7604f3b5f5ef0f6d186cc1c3c3d258a46a149
Shreesha_Voniyadka <shreesha@assertion.cloud>
2023-12-07 20:03:27 +0530

    Constistence is the key

--- src/Navigationbar/navigationbar.js
+++ src/Navigationbar/navigationbar.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './nav.css'
 
 const NavigationBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          {/* <Link to="/">Home</Link>  */}
-          <a href="/">Home</a>
-        </li>
-        <li>
-          {/* <Link to="/About">About</Link> */}
-          <a href="/About">About</a>
-        </li>
-        <li>
-          {/* <Link to="/Resume">Resume</Link> */}
-          <a href="/Resume">Resume</a>
-        </li>
-        <li>
-          {/* <Link to="/Projects">Projects</Link> */}
-          <a href="/Projects">Projects</a>
-        </li>
-        {/* Add more navigation items as needed */}
-      </ul>
-    </nav>
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       {/* <Link to="/">Home</Link>  */}
+    //       <a href="/">Home</a>
+    //     </li>
+    //     <li>
+    //       {/* <Link to="/About">About</Link> */}
+    //       <a href="/About">About</a>
+    //     </li>
+    //     <li>
+    //       {/* <Link to="/Resume">Resume</Link> */}
+    //       <a href="/Resume">Resume</a>
+    //     </li>
+    //     <li>
+    //       {/* <Link to="/Projects">Projects</Link> */}
+    //       <a href="/Projects">Projects</a>
+    //     </li>
+    //     {/* Add more navigation items as needed */}
+    //   </ul>
+    // </nav>
+    <header >
+
+      <nav>
+        <ul>
+          <li><a href="/"><i class="icon-home"></i>Home</a></li>
+          <li><a href="/about"><i class="icon-user"></i>About</a></li>
+          <li><a href="/projects"><i class="icon-thumbs-up-alt"></i>Projects</a></li>
+          <li><a href="/resume"><i class="icon-gear"></i>Resume</a></li>
+          {/* <li><a href="#gallery"><i class="icon-picture"></i>Gallery</a></li> */}
+          {/* <li><a href="#contact"><i class="icon-phone"></i>Contact</a></li> */}
+        </ul>
+      </nav>
+    </header>
   );
 };
 
