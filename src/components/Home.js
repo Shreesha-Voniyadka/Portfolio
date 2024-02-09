0cb62989066e3c519a763858bccaa89dfd464d87
ShreeshaVoniyadka <shreeshavoniyadka@gmail.com>
2024-02-09 16:55:09 +0530

    2/9/2024

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import "./Styles/Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>ポートフォリオ</h1>
+      <div className="Container">
+        <div className="Card">
+          <p>
+            Sint minim non tempor excepteur nulla adipisicing excepteur minim
+            eiusmod. Magna ullamco nulla proident id nulla fugiat ea quis et
+            minim. Nulla sint nisi aliquip ipsum ut irure duis eu. Lorem aliqua
+            veniam tempor id dolor amet sunt mollit ea nostrud nostrud
+            incididunt. Lorem veniam cillum voluptate voluptate enim in aliqua
+            sit incididunt laboris quis. Enim labore et magna do Lorem tempor
+            aliquip mollit officia quis cillum eu est magna. Ad fugiat occaecat
+            fugiat sunt irure exercitation non culpa quis.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
