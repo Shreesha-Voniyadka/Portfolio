016aeb984780a006ff5ee99d25953fc85c6aeb3b
ShreeshaVoniyadka <shreeshavoniyadka@gmail.com>
2024-02-21 19:55:29 +0530

    2/21/2024

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Project from "./components/Project";
+import Resume from "./components/Resume";
 import NavigationBar from "./components/NavigationBar";
 function App() {
   return (
@@ -10,7 +13,9 @@ function App() {
         <NavigationBar />
         <Routes>
           <Route path="/" element={<Home />}>
-          <Route path="/about" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/resume" element={<Resume />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -13,6 +13,12 @@ export class NavigationBar extends Component {
           <li>
             <Link to={"/about"}>About</Link>
           </li>
+          <li>
+            <Link to={"/projects"}>Projects</Link>
+          </li>
+          <li>
+            <Link to={"/resume"}>Resume</Link>
+          </li>
         </ul>
       </nav>
     );
