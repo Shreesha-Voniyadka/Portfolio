23397c4a5f08729d5904ee21cc2acc0acbb065ff
ShreeshaVoniyadka <shreeshavoniyadka@gmail.com>
2024-03-03 23:05:35 +0530

    3/3/2024

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import "./index.css";
-
+import { TypeAnimation } from "react-type-animation";
+import gif from "../Assets/Images/luffy-one-piece-anime.gif";
 function Home() {
   return (
     <div className="Homebody">
-      <h1>Home</h1>
+      <div className="container">
+        <h1>Hello, Myself Shreesha Voniyadka</h1>
+        <p>
+          I am a{" "}
+          <TypeAnimation
+            sequence={[
+              "Software Developer",
+              500,
+              "Backend Developer",
+              500,
+              "Full Stack Developer",
+              500,
+              "Software Engineer",
+              500,
+            ]}
+            repeat={Infinity}
+          />
+        </p>
+        <img src={gif} alt="my-gif" className="gifcontainer"/>
+      </div>
     </div>
   );
 }
